daa8aca6adedea0c720ffb92c9261296064c72aa
jjq <zhh1>
2014-07-28 13:54:04 +0800

    write the judgment function

--- app/scripts/controllers/create_activity.js
+++ app/scripts/controllers/create_activity.js
@@ -7,25 +7,21 @@ angular.module('PartyBidApp')
 
 
         $scope.confirm_create=function(){
+
             var present_name=$scope.activity_name;
 
             if(localStorage.length!=0)   //判断localStorage里有没有存储活动
             {
-                var i=0;
                 var activity=JSON.parse(localStorage.getItem("activity_names"));
+                i=judgment(activity,present_name);
 
-                for(var n=0;n<activity.length;n++) {    //遍历localStorage里的活动，查看是否有重复的名称
-                    if (activity[n] == present_name) {
-                        $scope.tip = "*活动名称重复";
-                        i=1;
-                        break;
-                    }
-                }
                 if(i==0){              //i==0说明localStorage里有活动，但与新创建的活动名称不重复
-
                    activity.unshift(present_name);  //unshift将新创建的活动存在数组的最开头部分，方便活动列表里的活动排序
                    localStorage.setItem("activity_names",JSON.stringify(activity));
                    $location.path('/sign_up');
+                }
+                else{
+                    $scope.tip="*活动名称重复";
                 }
 
             }
@@ -38,5 +34,18 @@ angular.module('PartyBidApp')
             }
 
 
+        }
+
+        function judgment(array,name) {
+
+            var i=0;
+            for (var n = 0; n < array.length; n++) {    //遍历localStorage里的活动，查看是否有重复的名称
+                if (array[n] ==name) {
+
+                    i = 1;
+
+                }
+            }
+            return i;
         }
     });
