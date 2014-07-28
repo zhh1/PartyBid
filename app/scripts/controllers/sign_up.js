9171e9f92b801f6a231d221d505b0317590cd921
jjq <zhh1>
2014-07-28 16:30:11 +0800

    change the state of button(start_or_end) of sign_up.html

--- app/scripts/controllers/sign_up.js
+++ app/scripts/controllers/sign_up.js
@@ -5,5 +5,21 @@ angular.module('PartyBidApp')
     .controller('SignUpCtrl', function ($scope, $location) {
         $scope.back_to_activity_list=function(){$location.path('/activity_list')}
 
+        $scope.start_or_end="开始";
+
+        $scope.start_sign_up=function(){
+            if($scope.start_or_end=="开始"){
+                $scope.start_or_end="结束";
+            }
+            else{
+                $scope.start_or_end="开始";
+            }
+
+
+        }
+
+
+
+
 
     });
