11da48cb7fa8195d638c0b53a926c66f30e978ad
jjq <zhh1>
2014-08-15 10:12:36 +0800

    refactor price_messages.js with underscore

--- app/scripts/model/price_messages.js
+++ app/scripts/model/price_messages.js
@@ -16,31 +16,21 @@ PriceMessages.get_price_messages = function() {
 
 PriceMessages.is_someone_sign_up_succeed = function(person_phone) {
     var current_activity_messages = Message.get_current_activity_messages(Price.get_price_signing_up());
-    for(var i = 0;i < current_activity_messages.length;i++) {
-        if(current_activity_messages[i].phone == person_phone) {
-            return true;
-        }
-    }
-    return false;
+    return _.some(current_activity_messages,function(item) {
+        return item.phone == person_phone;
+    });
 };
 
 PriceMessages.get_current_price_messages = function(price) {
-    var current_price_messages = [];
-    for(var i = 0;i < PriceMessages.get_price_messages().length;i++) {
-        if(PriceMessages.get_price_messages()[i].activity_name == price.activity_name && PriceMessages.get_price_messages()[i].price_name == price.price_name) {
-            current_price_messages.push(PriceMessages.get_price_messages()[i]);
-        }
-    }
-    return current_price_messages;
+    var price_messages = PriceMessages.get_price_messages();
+    return _.where(price_messages,{activity_name:price.activity_name,price_name:price.price_name});
 };
 
 PriceMessages.is_price_phone_repeat = function(person_phone,price) {
-    for(var i = 0;i< PriceMessages.get_current_price_messages(price).length;i++) {
-        if(PriceMessages.get_current_price_messages(price)[i].phone == person_phone) {
-            return false;
-        }
-    }
-    return true;
+    var current_price_messages = PriceMessages.get_current_price_messages(price);
+    return !_.some(current_price_messages,function(item) {
+        return item.phone == person_phone;
+    });
 };
 
 PriceMessages.prototype.save = function() {
